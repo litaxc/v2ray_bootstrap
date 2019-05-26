46e971f5c055da5dba120fbd488647a839891859
lita <fireice47@gmail.com>
2019-05-26 19:37:10 +0800

    move env var to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,16 @@
+export __ADDRESS__=
+
+export __UUID__=$(uuidgen)
+export V2RAY_LOCATION_ASSET=.
+export V2RAY_LOCATION_CONFIG=./server
+
 sudo apt-get update
 sudo apt-get install -y unzip
 
 curl -o v2ray.zip -L https://github.com/v2ray/v2ray-core/releases/download/v4.18.2/v2ray-linux-64.zip
 unzip v2ray.zip
 
-source ./config.env
-sed -i 's/__UUID__/'"${__UUID__}"'/g; s/__ADDRESS__/'"${__ADDRESS__}"'/g' local/config.json
-sed -i 's/__UUID__/'"${__UUID__}"'/g; s/__ADDRESS__/'"${__ADDRESS__}"'/g' server/config.json
+sed -i 's/__UUID__/'"${__UUID__}"'/g; s/__ADDRESS__/'"${__ADDRESS__}"'/g' ./local/config.json
+sed -i 's/__UUID__/'"${__UUID__}"'/g; s/__ADDRESS__/'"${__ADDRESS__}"'/g' ./server/config.json
 
 nohup ./v2ray &
