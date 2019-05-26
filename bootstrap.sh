db85a7ebbad67581905ae80300933cdd90356e30
lita <fireice47@gmail.com>
2019-05-26 19:52:33 +0800

    remove default config.json to prevent it being load

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ sudo apt-get install -y unzip
 
 curl -o v2ray.zip -L https://github.com/v2ray/v2ray-core/releases/download/v4.18.2/v2ray-linux-64.zip
 unzip v2ray.zip
+rm config.json
 
 sed -i 's/__UUID__/'"${__UUID__}"'/g; s/__ADDRESS__/'"${__ADDRESS__}"'/g' ./local/config.json
 sed -i 's/__UUID__/'"${__UUID__}"'/g; s/__ADDRESS__/'"${__ADDRESS__}"'/g' ./server/config.json
